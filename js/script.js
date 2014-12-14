3c4b97f5bf80924ec26b0a983b467b98aa210bea
Christopher Reid <chronic88@gmail.com>
2014-12-14 00:01:34 -0800

    using indexedDB to store image, TODO: smooth load

--- js/script.js
+++ js/script.js
@@ -20,55 +20,16 @@ dragList.addListener();
 var backgroundButton = document.querySelector('#background-button');
 var backgroundUpload = document.querySelector('#background-upload');
 
-backgroundButton.addEventListener('click', function(){ backgroundUpload.click(); }, false);
-backgroundUpload.addEventListener('change', function(e){
-
-  var file = e.target.files[0];
-
-  var openDB = window.indexedDB.open("good-tab", 1)
-
-  openDB.onsuccess = function(e){
-    console.log("opened");
-    db = e.target.result;
-    var store = db.transaction(['background-image'], 'readwrite').objectStore('background-image');
-    var req = store.put(file, 'file');
-    var url = URL.createObjectURL(file);
-    document.body.style.background = "url("+url+") no-repeat center center fixed";
-    document.body.style.webkitBackgroundSize = "cover";
-  }
-
-    // var store = db.transaction(['background-image'], 'readwrite').objectStore('background-image');
-    // var req = store.put(file, 'file');
-    // console.log(req);
-
-
-
-    // var store = db.transaction(['good-tab'], 'readwrite').objectStore('good-tab');
-    // var req = store.put(file, 'background-image');
-
-    // console.log(req);
-
-  // var reader = new FileReader();
-  // reader.readAsDataURL(e.target.files[0]);
-  // reader.onloadend = function(e){
-  //   localStorage["good-tab-background-image"] = e.target.result
-  //   document.body.style.background = "url("+localStorage["good-tab-background-image"]+") no-repeat center center fixed";
-  //   document.body.style.webkitBackgroundSize = "cover";
-  // }
-}, false);
-
+var db;
 var openDB = window.indexedDB.open("good-tab", 1)
 
 openDB.onupgradeneeded = function(e){
-
   console.log("initialized");
   db = e.target.result;
   var objectStore = db.createObjectStore("background-image");
-
 }
 
 openDB.onsuccess = function(e){
-
   db = e.target.result;
   var transaction = db.transaction(["background-image"]);
   var objectStore = transaction.objectStore("background-image");
@@ -84,5 +45,17 @@ openDB.onsuccess = function(e){
       document.body.style.webkitBackgroundSize = "cover";
     }
   };
-
 }
+
+backgroundButton.addEventListener('click', function(){ backgroundUpload.click(); }, false);
+backgroundUpload.addEventListener('change', function(e){
+  var file = e.target.files[0];
+  var store = db.transaction(['background-image'], 'readwrite').objectStore('background-image');
+  var req = store.put(file, 'file');
+  var url = URL.createObjectURL(file);
+  document.body.style.transition = "1s";
+  document.body.style.background = "url("+url+") no-repeat center center fixed";
+  document.body.style.webkitBackgroundSize = "cover";
+}, false);
+
+document.body.onload = function(){ console.log("DONE"); }
